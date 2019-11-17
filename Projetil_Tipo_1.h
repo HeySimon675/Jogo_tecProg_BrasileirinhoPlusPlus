5fd41f62b78852cb7bc26d3b3648bb03c9103749
ViniCora <viniciuscoradassi@hotmail.com>
2019-11-17 20:40:46 -0300

    Update Projetil_Tipo_1.h

--- Projetil_Tipo_1.h
+++ Projetil_Tipo_1.h
@@ -15,5 +15,17 @@
 //Classe Projetil 1//
 class Projetil_Tipo_1 : public Projetil {
 
+private:
+    float posicao_X_inicial;
+    bool dead;
+
+public:
+    Projetil_Tipo_1(sf::Vector2f position, sf::Vector2f size, bool active, float speed);
+    Projetil_Tipo_1();
+    ~Projetil_Tipo_1();
+
+    void inicializaProjetilTipo1(sf::Vector2f position, sf::Vector2f size, bool active, float speed);
+    void calculaMovimento();
+    bool getDead() {return dead;}
 };
 
