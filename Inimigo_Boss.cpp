e99661d963b1789dbf8900c2f932db4e6faf1528
Simao <gabrielsimao@hotmail.com>
2019-11-22 00:24:40 -0300

    Classe Inimigo_Boss Refinada

--- Inimigo_Boss.cpp
+++ Inimigo_Boss.cpp
@@ -1,5 +1,67 @@
 
 //--------------------------------------------------------------------------------------------------------------------//
-// Created by simao on 11/11/2019.
-
 #include "Inimigo_Boss.h"
+
+Inimigo_Boss::Inimigo_Boss(Vector2f position, Vector2f size, float speed) : Inimigo(position,size,speed)
+{
+    position_X_inicial = position.x;
+    goingLeft = true;
+    canMove = true;
+    projetilCriado = false;
+}
+
+Inimigo_Boss::~Inimigo_Boss()
+{
+
+}
+
+void Inimigo_Boss::inicializarBoss(Vector2f position)
+{
+    setPosition(position);
+}
+
+void Inimigo_Boss::calculaMovimento(const float deltaTime)
+{
+    if(!projetilCriado)
+    {
+        projetil->setPosicao(Vector2f(body.getPosition().x, body.getPosition().y - (body.getSize().y/4.0f)));
+        projetilCriado = true;
+    }
+
+    if(projetil->getDead() && canMove)
+    {
+        projetil->setPosicao(Vector2f(body.getPosition().x, body.getPosition().y - (body.getSize().y/4.0f)));
+        vel.x = 0;
+        if(goingLeft)
+        {
+            if(!(body.getPosition().x <= (position_X_inicial - 548.0f) && body.getPosition().x >= (position_X_inicial - 552.0f)))
+            {
+                vel.x -= speed;
+            }
+            else
+            {
+                goingLeft = false;
+                //canMove = false;
+                projetil->setDead(false);
+                projetil->setLeft(false);
+                // vel.x = 0;
+            }
+        }
+
+        else{
+
+            if(!(body.getPosition().x >= (position_X_inicial - 2.0f) && body.getPosition().x <= position_X_inicial +2.0f))
+            {
+                vel.x += speed;
+            }
+            else
+            {
+                goingLeft = true;
+                projetil->setDead(false);
+                projetil->setLeft(true);
+                // vel.x = 0;
+            }
+        }
+    }
+}
+
--- Inimigo_Boss.h
+++ Inimigo_Boss.h
@@ -1,7 +1,9 @@
 
 //--------------------------------------------------------------------------------------------------------------------//
-//Created by simao on 11/11/19.
-//Last Update 11/11 12hrs
+//Implementado por:
+//Coradassi - 90%
+//Simao     - 10%
+
 #pragma once
 //--------------------------------------------------------------------------------------------------------------------//
 //Herança de Inimigo
@@ -9,9 +11,24 @@
 //--------------------------------------------------------------------------------------------------------------------//
 //Header//
 #include "Inimigo.h"
+#include "Projetil.h"
 
 //--------------------------------------------------------------------------------------------------------------------//
 //Classe Inimigo_Boss//
 class Inimigo_Boss : public Inimigo{
+public:
+    Inimigo_Boss(Vector2f position = POSICAO_PADRAO, Vector2f size = TAMANHO_PADRAO,  float speed = VELOCIDADE_PADRAO);
+    ~Inimigo_Boss();
 
+    void inicializarBoss(Vector2f position);
+    void calculaMovimento(const float deltaTime);
+    void setProjetil(Projetil* projetil){this->projetil = projetil;}
+
+private:
+    Projetil* projetil;
+    float position_X_inicial;
+    bool goingLeft;
+    bool canMove;
+    float delta_X;
+    bool projetilCriado;
 };
