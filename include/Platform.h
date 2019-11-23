207ade940628e21673fcae5183ebe4fdbc4c756f
Simão <gabrielsimao@hotmail.com>
2019-11-23 17:50:01 -0300

    Removendo arquivos

--- include/Platform.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include "Entity.h"
-class Platform : public Entity
-{
-public:
-	Platform(const char* textureFile = NULL);
-	~Platform();
-	void InitializePlatform(sf::Vector2f position, sf::Vector2f size, const char* textureFile);
-};
